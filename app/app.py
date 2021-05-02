b6592edab19dd91f38e89e2645d79f861d9108c1
Nalron <43935337+nalron@users.noreply.github.com>
2021-05-03 00:00:10 +0200

    first commit application v1

---
+++ app/app.py
@@ -0,0 +1,179 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+from zipfile import ZipFile
+from sklearn.cluster import KMeans
+plt.style.use('fivethirtyeight')
+#sns.set_style('darkgrid')
+
+
+def main() :
+
+    @st.cache
+    def load_data():
+        z = ZipFile("data/default_risk.zip")
+        data = pd.read_csv(z.open('default_risk.csv'), index_col='SK_ID_CURR', encoding ='utf-8')
+
+        z = ZipFile("data/X_sample.zip")
+        sample = pd.read_csv(z.open('X_sample.csv'), index_col='SK_ID_CURR', encoding ='utf-8')
+
+        target = data.iloc[:, -1:]
+
+        return data, sample, target
+
+
+    def load_model():
+        '''loading the trained model'''
+        pickle_in = open('model/LGBMClassifier.pkl', 'rb') 
+        clf = pickle.load(pickle_in)
+        return clf
+
+
+    @st.cache(allow_output_mutation=True)
+    def load_knn(sample):
+        knn = knn_training(sample)
+        return knn
+
+
+    @st.cache
+    def load_infos_gen(data):
+        lst_infos = [data.shape[0],
+                     round(data["AMT_INCOME_TOTAL"].mean(), 2),
+                     round(data["AMT_CREDIT"].mean(), 2)]
+
+        nb_credits = lst_infos[0]
+        rev_moy = lst_infos[1]
+        credits_moy = lst_infos[2]
+
+        targets = data.TARGET.value_counts()
+
+        return nb_credits, rev_moy, credits_moy, targets
+
+
+    def identite_client(data, id):
+        data_client = data[data.index == int(id)]
+        return data_client
+
+    @st.cache
+    def load_age_population(data):
+        data_age = round((data["DAYS_BIRTH"]/365), 2)
+        return data_age
+
+    @st.cache
+    def load_income_population(sample):
+        df_income = pd.DataFrame(sample["AMT_INCOME_TOTAL"])
+        df_income = df_income.loc[df_income['AMT_INCOME_TOTAL'] < 200000, :]
+        return df_income
+
+    def load_prediction(sample, id, clf):
+        X=sample.iloc[:, :-1]
+        score = clf.predict_proba(X[X.index == int(id)])[:,1]
+        return score
+
+    def load_kmeans(sample, id, mdl):
+        index = sample[sample.index == int(id)].index.values
+        index = index[0]
+        data_client = pd.DataFrame(sample.loc[sample.index, :])
+        df_neighbors = pd.DataFrame(knn.fit_predict(data_client), index=data_client.index)
+        df_neighbors = pd.concat([df_neighbors, data], axis=1)
+        return df_neighbors.sample(10)
+
+    def knn_training(sample):
+        knn = KMeans(n_clusters=2).fit(sample)
+        return knn 
+
+
+
+    #Loading data……
+    data, sample, target = load_data()
+    id_client = sample.index.values
+    clf = load_model()
+
+
+    #######################################
+    # SIDEBAR
+    #######################################
+
+    #Title display
+    html_temp = """
+    <div style="background-color: tomato; padding:10px; border-radius:10px">
+    <h1 style="color: white; text-align:center">Dashboard Scoring Credit</h1>
+    </div>
+    <p style="color: white; font-size: 20px; font-weight: bold; text-align:center">Credit decision support…</p>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    #Customer ID selection
+    st.sidebar.header("**General Info**")
+
+    #Loading selectbox
+    chk_id = st.sidebar.selectbox("Client ID", id_client)
+
+    #Loading general info
+    nb_credits, rev_moy, credits_moy, targets = load_infos_gen(data)
+
+
+    ### Display of information in the sidebar ###
+    #Number of loans in the sample
+    st.sidebar.markdown("<u>Number of loans in the sample :</u>", unsafe_allow_html=True)
+    st.sidebar.text(nb_credits)
+
+    #Average income
+    st.sidebar.markdown("<u>Average income (USD) :</u>", unsafe_allow_html=True)
+    st.sidebar.text(rev_moy)
+
+    #AMT CREDIT
+    st.sidebar.markdown("<u>Average loan amount (USD) :</u>", unsafe_allow_html=True)
+    st.sidebar.text(credits_moy)
+
+    #######################################
+    # HOME PAGE - MAIN CONTENT
+    #######################################
+    #Display Customer ID from Sidebar
+    st.write("Customer ID selection :", chk_id)
+
+
+    #Customer information display : Customer Gender, Age, Family status, Children, …
+    st.header("**Customer information display**")
+
+    if st.checkbox("Show customer information ?"):
+
+        infos_client = identite_client(data, chk_id)
+        st.write("Gender :**", infos_client["CODE_GENDER"].values[0], "**")
+        st.write("Age :", int(infos_client["DAYS_BIRTH"]/365), "ans")
+        st.write("Family status :**", infos_client["NAME_FAMILY_STATUS"].values[0], "**")
+        st.write("Number of children :**", infos_client["CNT_CHILDREN"].values[0], "**") 
+
+        st.subheader("*Income (USD)*")
+        st.write("Income total :", infos_client["AMT_INCOME_TOTAL"].values[0])
+        st.write("Credit amount :", infos_client["AMT_CREDIT"].values[0])
+        st.write("Credit annuities :", infos_client["AMT_ANNUITY"].values[0])
+        st.write("Amount of property for credit :", infos_client["AMT_GOODS_PRICE"].values[0])
+    else:
+        st.markdown("<i>…</i>", unsafe_allow_html=True)
+
+    #Customer solvability display
+    st.header("**Customer file analysis**")
+    st.markdown("<u>Default probability :</u>", unsafe_allow_html=True)
+    prediction = load_prediction(sample, chk_id, clf)
+    st.write(round(float(prediction)*100, 2), "%")
+
+    st.markdown("<u>Customer Data :</u>", unsafe_allow_html=True)
+    st.write(identite_client(data, chk_id)) 
+
+    #Similar customer files display
+    chk_voisins = st.checkbox("Show similar customer files ?")
+
+    if chk_voisins:
+        knn = load_knn(sample)
+        st.markdown("<u>List of the 10 files closest to this Customer :</u>", unsafe_allow_html=True)
+        st.dataframe(load_kmeans(sample, chk_id, knn))
+        st.markdown("<i>Target 1 = Customer with default</i>", unsafe_allow_html=True)
+    else:
+        st.markdown("<i>…</i>", unsafe_allow_html=True)
+
+if __name__ == '__main__':
+    main()
